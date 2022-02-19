b9f3c13a2d9cab25ba8e959e5f7df995b4969230
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-02-19 17:27:18 +0300

    random practice uploads

---
+++ trial.js
@@ -0,0 +1,15 @@
+function showMyTest()
+{
+
+    document.getElementById("text1").innerHTML="Hello Am Jeff !!!!"
+    
+}
+
+
+function changeColor()
+{
+    document.getElementById("text1").style.color="red";//button will change the color o text1 
+
+}
+
+
